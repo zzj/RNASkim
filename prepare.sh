c609b05c61da3a2642168b4ecf1791cdf492a227
Zhaojun Zhang <zhangzhaojun@gmail.com>
2014-04-20 19:45:32 -0400

    Minor changes

---
+++ prepare.sh
@@ -0,0 +1,44 @@
+# TODO: rewite everything in Python
+# It is easier to maintain and incorporate with my other scripts.
+
+export CXX=g++
+export RS_HOME=`pwd`
+export LDFLAGS="-L$RS_HOME/lib/gflags-2.0/.libs/ -L$RS_HOME/lib/glog-0.3.3/.libs/"
+export CPPFLAGS="-I$RS_HOME/lib/gflags-2.0/src/  -I$RS_HOME/lib/glog-0.3.3/src/"
+
+mkdir -p lib
+cd lib
+wget --no-check-certificate  https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.gz
+tar zxf protobuf-2.5.0.tar.gz
+cd protobuf-2.5.0
+./configure --prefix=`pwd`/../../third_party/
+make -j8
+make install
+cd python
+python setup.py build
+python setup.py install
+cd ..
+cd ..
+
+pip install leveldb mock protobuf
+
+# install gtest
+
+wget http://googletest.googlecode.com/files/gtest-1.7.0.zip
+unzip gtest-1.7.0.zip
+
+wget  --no-check-certificate https://gflags.googlecode.com/files/gflags-2.0.zip
+unzip gflags-2.0.zip
+cd gflags-2.0
+./configure
+make
+cd ..
+
+wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
+tar zxf glog-0.3.3.tar.gz
+cd glog-0.3.3
+./configure
+make
+
+cd ..
+cd ..
