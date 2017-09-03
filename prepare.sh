e989e4cd4db622f5464006cbeb3b20aea5036941
chelseaju <chelseaju@ucla.edu>
2017-09-02 22:23:12 -0700

    change directory files for make
    update directory names to be compatible withe the Makefile

--- prepare.sh
+++ prepare.sh
@@ -29,6 +29,7 @@ pip install leveldb protobuf pyfasta --user
 wget https://github.com/google/googletest/archive/release-1.7.0.zip
 mv release-1.7.0.zip gtest-1.7.0.zip
 unzip gtest-1.7.0.zip
+mv googletest-release-1.7.0 gtest-1.7.0
 
 #wget  --no-check-certificate https://gflags.googlecode.com/files/gflags-2.0.zip
 wget  --no-check-certificate https://github.com/gflags/gflags/archive/v2.0.zip
@@ -45,7 +46,8 @@ cd ..
 wget  --no-check-certificate https://github.com/google/glog/archive/v0.3.4.tar.gz
 mv v0.3.4.tar.gz glog-0.3.4.tar.gz
 tar zxf glog-0.3.4.tar.gz
-cd glog-0.3.4
+mv glog-0.3.4 glog-0.3.3
+cd glog-0.3.3
 ./configure
 make
 
